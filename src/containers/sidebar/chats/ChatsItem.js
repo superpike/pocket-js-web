58ea73ecc724a532d5a88199715de27cc700b6e2
Kozlovtseva <alina.lm@mail.ru>
2019-04-02 09:08:57 +0300

    add test for ChatsItem.js

--- src/containers/sidebar/chats/ChatsItem.js
+++ src/containers/sidebar/chats/ChatsItem.js
@@ -1,40 +1,35 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 import styles from './ChatsItem.module.css';
 
-export default class ChatsItem extends Component {
-  render() {
-    // const ChatsItem = (props)  => {
-    return (
-      <div className={styles.Block} onClick={() => this.props.handleClicks(this.props.id)}>
-        <NavLink to={`/chats/${this.props.id}`}>
-          <div className={styles.Item}>
-            <div className={styles.Img}>
-              <img src='http://simsontraining.com/wp-content/uploads/2015/03/testimonial_icon-60x60.png' className={styles.Icon} alt="group_icon" />
-            </div>
 
-            <div className={styles.Chat}>
-              <div className={styles.Title}>
-                {this.props.name}
-              </div>
-              <div className={styles.Text}>
-                {this.props.preview}
-              </div>
+const ChatsItem = props => (
+    <div id="ChatsItem" className={styles.Block} onClick={() => props.handleClicks(props.id)}>
+        <NavLink to={`/chats/${props.id}`}>
+            <div className={styles.Item}>
+                <div className={styles.Img}>
+                    <img src="http://simsontraining.com/wp-content/uploads/2015/03/testimonial_icon-60x60.png" className={styles.Icon} alt="group_icon" />
+                </div>
+                <div className={styles.Chat}>
+                    {/* <div className={styles.Title}>
+                        {props.name}
+                    </div> */}
+                    <div className={styles.Text}>
+                        {props.preview}
+                    </div>
+                </div>
+                {/* <div className={styles.Unread}>
+                    {props.unread}
+                </div> */}
+                {/* <div className={styles.Time}>
+                    {props.time}
+                </div> */}
+                {/* <div className={styles.Mark}>
+                </div> */}
             </div>
-            <div className={styles.Unread}>
-              {this.props.unread}
-            </div>
-            <div className={styles.Time}>
-              {this.props.time}
-            </div>
-            <div className={styles.Mark}>
-              {/* props.mark */}
-            </div>
-          </div>
         </NavLink>
-      </div>
-    );
-  }
-}
-// export default ChatsItem;
+    </div>
+);
+
+export default ChatsItem;
